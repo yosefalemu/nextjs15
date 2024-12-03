2e360d372bd4d0d508cfea8ae2c9e4a20ce8d8ba
yosefalemu <yosefalemu007@gmail.com>
2024-12-03 16:50:29 +0300

    connection to postgress is done

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
-import mobileBackgroundImage from "@/public/hero-mobile.png";
-import desktopBackgroundImage from "@/public/hero-desktop.png";
 
 export default function Page() {
   return (
@@ -32,18 +30,18 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
-            src={desktopBackgroundImage}
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
-            priority
           />
           <Image
-            src={mobileBackgroundImage}
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
             className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
-            priority
-            placeholder="blur"
-            quality={100}
           />
         </div>
       </div>
